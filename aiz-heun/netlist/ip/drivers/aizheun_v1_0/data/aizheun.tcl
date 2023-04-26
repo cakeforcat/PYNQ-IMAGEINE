e2828aae000168e8d3c3b59f38241786310eb4d2
cakeforcat <jakub.czarny.2020@uni.strath.ac.uk>
2023-04-26 16:29:09 +0100

    restructuring start

--- aiz-heun/netlist/ip/drivers/aizheun_v1_0/data/aizheun.tcl
+++
@@ -1,6 +0,0 @@
-proc generate {drv_handle} {
-    xdefine_include_file $drv_handle "xparameters.h" "aizheun" "NUM_INSTANCES" "DEVICE_ID" "C_AIZHEUN_S_AXI_BASEADDR" "C_AIZHEUN_S_AXI_HIGHADDR" 
-    xdefine_config_file $drv_handle "aizheun_g.c" "aizheun" "DEVICE_ID" "C_AIZHEUN_S_AXI_BASEADDR" 
-    xdefine_canonical_xpars $drv_handle "xparameters.h" "aizheun" "DEVICE_ID" "C_AIZHEUN_S_AXI_BASEADDR" "C_AIZHEUN_S_AXI_HIGHADDR" 
-
-}
